0d94af2093ea57ff905b2a1559bc129d58ec60e6
Islam Hany <ihhg.developer@gmail.com>
2024-03-28 19:54:58 +0200

    0. Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+0-stats.py
+"""
+import signal
+import sys
+import re
+
+pattern = r'^(\S+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)'
+
+total_file_size = 0
+stats = {
+    "200": 0, "301": 0, "400": 0, "401": 0,
+    "403": 0, "404": 0, "405": 0, "500": 0,
+}
+line_count = 0
+
+
+def print_statistics():
+    """
+    Prints statistics.
+    """
+    print(f"File size: {total_file_size}")
+
+    for k, v in stats.items():
+        print(f"{k}: {v}")
+
+
+def signal_handler(sig, frame):
+    """
+    Ctrl-C handler.
+    """
+    print_statistics()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        match = re.match(pattern, line)
+
+        if line_count > 0 and line_count % 10 == 0:
+            print_statistics()
+
+        if match:
+            status_code = match.group(3)
+            file_size = match.group(4)
+
+            stats[status_code] += 1
+            total_file_size += int(file_size)
+            line_count += 1
