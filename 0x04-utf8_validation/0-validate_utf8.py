5b439f7201f3002052601182272bbebf91f6520b
Islam Hany <ihhg.developer@gmail.com>
2024-04-04 16:04:00 +0200

    fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,36 +4,32 @@
 """
 
 
-def utf8_control_bits(byte):
-    """
-    Returns utf-8 control bits.
-    """
-    control_bits = 1
-
-    while byte & 0b10000000:
-        if control_bits == 5:
-            return 0
-
-        control_bits += 1
-        byte = byte << 1
-
-    return control_bits
-
-
 def validUTF8(data):
     """
     Returns true if data is a valid utf-8 encoding else false.
     """
-    for byte_idx in range(len(data)):
-        sequence = utf8_control_bits(data[byte_idx]) - 1
+    in_sequence = 0
 
-        if sequence == -1:
-            return False
+    for byte in data:
+        if not in_sequence and byte >> 7 == 0b00000000:
+            continue
 
-        for _ in range(sequence):
-            byte_idx += 1
+        if not in_sequence and byte >> 3 == 0b00011110:
+            in_sequence = 3
+            continue
 
-            if utf8_control_bits(data[byte_idx]) != 2:
-                return False
+        if not in_sequence and byte >> 4 == 0b00001110:
+            in_sequence = 2
+            continue
+
+        if not in_sequence and byte >> 5 == 0b00000110:
+            in_sequence = 1
+            continue
+
+        if in_sequence and byte >> 6 == 0b00000010:
+            in_sequence -= 1
+            continue
+
+        return False
 
     return True
