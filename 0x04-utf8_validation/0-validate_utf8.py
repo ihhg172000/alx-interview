4f9696fb09cd7e576db61f05362c05d56de4c079
Islam Hany <ihhg.developer@gmail.com>
2024-04-05 01:47:51 +0200

    fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,25 +11,29 @@ def validUTF8(data):
     in_sequence = 0
 
     for byte in data:
-        if not in_sequence and byte >> 7 == 0b00000000:
-            continue
+        if not in_sequence:
+            if byte >> 7 == 0b00000000:
+                continue
 
-        if not in_sequence and byte >> 3 == 0b00011110:
-            in_sequence = 3
-            continue
+            if byte >> 3 == 0b00011110:
+                in_sequence = 3
+                continue
 
-        if not in_sequence and byte >> 4 == 0b00001110:
-            in_sequence = 2
-            continue
+            if byte >> 4 == 0b00001110:
+                in_sequence = 2
+                continue
 
-        if not in_sequence and byte >> 5 == 0b00000110:
-            in_sequence = 1
-            continue
+            if byte >> 5 == 0b00000110:
+                in_sequence = 1
+                continue
 
         if in_sequence and byte >> 6 == 0b00000010:
             in_sequence -= 1
             continue
 
+        return False
+
+    if in_sequence:
         return False
 
     return True
