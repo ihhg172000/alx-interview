f001b7314c29f49f2a3067637730e5e87d619a19
Islam Hany <ihhg.developer@gmail.com>
2024-04-16 23:13:33 +0200

    error handling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,12 +1,26 @@
 #!/usr/bin/node
+const { argv, exit } = require('process');
 const request = require('request');
+
+class HTTPError extends Error {
+  constructor (statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
 
 const getRequest = async (url) => {
   return new Promise((resolve, reject) => {
-    request(url, (error, request, body) => {
+    request(url, (error, response, body) => {
       if (error) {
         reject(error);
       } else {
+        if (response.statusCode >= 400) {
+          const message = JSON.parse(body).detail;
+
+          reject(new HTTPError(response.statusCode, message));
+        }
+
         resolve(JSON.parse(body));
       }
     });
@@ -14,18 +28,32 @@ const getRequest = async (url) => {
 };
 
 const main = async () => {
-  const movieId = process.argv[2];
+  if (argv.length !== 3) {
+    console.log(`Usage: ${argv[1]} movieId`);
+    exit(1);
+  }
+
+  const movieId = argv[2];
   const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-  const movie = await getRequest(movieUrl);
-  const charactersUrls = movie.characters;
+  try {
+    const movie = await getRequest(movieUrl);
+    const charactersUrls = movie.characters;
 
-  const characters = await Promise.all(
-    charactersUrls.map((charactersUrl) => getRequest(charactersUrl))
-  );
+    const characters = await Promise.all(
+      charactersUrls.map((charactersUrl) => getRequest(charactersUrl))
+    );
 
-  for (const character of characters) {
-    console.log(character.name);
+    for (const character of characters) {
+      console.log(character.name);
+    }
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      console.log(error.statusCode, error.message);
+    } else {
+      console.error(error);
+      exit(1);
+    }
   }
 };
 
